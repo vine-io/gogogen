f75762b949e4ad8a98a8bf8fa8853580b03011bc
xingyys <598223084@qq.com>
2020-12-19 16:18:25 +0800

    from github.com/lack-io/vineadm

---
+++ cmd/set-gen/main.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The gogogen Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// set-gen is an example usage of gengo
+//
+// Struct in the input directories with the below line in their comments will
+// have sets generated for them.
+// // +vine:genset
+//
+// Any builtin type referenced anywhere in the input directories will have a
+// set generated for it.
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/lack-io/gogogen/gogenerator/args"
+	"github.com/lack-io/gogogen/set-gen"
+	utilbuild "github.com/lack-io/gogogen/util/build"
+	"github.com/lack-io/gogogen/util/log"
+)
+
+func main() {
+	arguments := args.Default()
+
+	// Override defaults.
+	arguments.GoHeaderFilePath = filepath.Join(args.DefaultSourceTree(), utilbuild.BoilerplatePath())
+	arguments.InputDirs = []string{"github.com/lack-io/gogogen/util/sets/types"}
+	arguments.OutputPackagePath = "github.com/lack-io/gogogen/util/sets"
+
+	if err := arguments.Execute(
+		set_gen.NameSystems(),
+		set_gen.DefaultNameSystem(),
+		set_gen.Packages,
+	); err != nil {
+		log.Errorf("Error: %v", err)
+		os.Exit(1)
+	}
+	log.Infof("Completed successfully.")
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/lack-io/gogogen
+
+go 1.15
+
+require (
+	github.com/lack-io/cli v1.0.2
+	go.uber.org/zap v1.16.0
+	golang.org/x/tools v0.0.0-20201218024724-ae774e9781d2
+)
